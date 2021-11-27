8d993934659bed6a3c0d8d7dfa3ae79d8aa6adda
bcharlotin1 <biancacharlotin1@gmail.com>
2021-11-27 09:57:47 -0500

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -7,9 +7,9 @@ export default function manageBand(
   
   switch (action.type) {
     case "ADD_BAND":
-      console.log({ bands: [...state.bands, action.payload.text] });
-
-      return { bands: [...state.bands, action.payload.text] };
+      console.log({ bands: [...state.bands, {name: action.payload.name}] });
+ 
+      return { bands: [...state.bands, {name: action.payload.name}] };
 
     default:
       return state;
